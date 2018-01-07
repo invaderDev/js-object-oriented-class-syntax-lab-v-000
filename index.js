1682caf0a327f6edffcb66e33067d4d2416f0b1c
Kris Thompson <kt.devcat@gmail.com>
2018-01-07 17:42:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ class BoardMember {
   }
 
   veto() {
-
+    return 'No, I must disagree';
   }
+
+  approve() {
+    return 'You can do that!';
+  }
+
+  doCharity() {
+    return "I like to help people.";
+  }
+
 }
